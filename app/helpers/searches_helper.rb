95a0f268cfb025d6d64ad1f2639a4e548c05a1cc
admin <admin@mail.com>
2014-06-30 00:13:12 -0400

    modify some issues

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,7 +1,7 @@
 module SearchesHelper
 	def get_average_score(str)
-		scores = DriveFeedback.where(:drive_id => str.to_i).sum(:score)
-		counts = DriveFeedback.where(:drive_id => str.to_i).count
+		scores = DriveFeedback.where(:drive_id => str.to_s).sum(:score)
+		counts = DriveFeedback.where(:drive_id => str.to_s).count
 		if scores == 0 || counts == 0
 			@average = 0
 		else
